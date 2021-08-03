7bfdb4d83b617bc114a885506cedc92c59dd472c
WangShouwei <740209668@qq.com>
2021-08-03 18:40:52 +0800

    'a'

--- demo.php
+++ demo.php
@@ -4,6 +4,23 @@ use wutongshenyuan\qrcode_prettify\QrcodeOptions;
 use wutongshenyuan\qrcode_prettify\Color\RGBA;
 use wutongshenyuan\qrcode_prettify\Qrcode;
 use wutongshenyuan\qrcode_prettify\Foreground\Foreground;
+
+$im = imagecreate(100, 100);
+$white = imagecolorallocate($im, 255, 255, 255);
+$black = imagecolorallocate($im, 0, 0, 0);
+imagefill($im, 0, 0, $white);
+
+imagefilledellipse(
+    $im,
+    50,
+    50,
+    50,
+    50,
+    $black
+);
+imagepng($im, __DIR__.'/src/temp/tuoyuan.png');
+imagedestroy($im);
+exit;
 $opt = new QrcodeOptions();
 $opt->setForegroundColor(new RGBA(0,255,0));
 $opt->setBackgroundColor(new RGBA(255,255,0));
--- src/Command/DotCommand.php
+++ src/Command/DotCommand.php
@@ -11,9 +11,11 @@ namespace wutongshenyuan\qrcode_prettify\Command;
 
 class DotCommand extends Command
 {
-    // 圆点效果
+    // 圆点效果 须设置大一点的$matrixPointSize否则圆点画不出来
+    // 须以二维码的一个点为处理单位
     public function execute($QR, $option)
     {
+        // 也可以按二维码的一个点为单位处理
 
         return $QR;
     }
