34ff92fae6286cae796fd3edd98b9921c2bd5131
WangShouwei <740209668@qq.com>
2021-08-10 14:20:01 +0800

    'QR_PNG_MAXIMUM_SIZE_4096'

--- src/phpqrcode.php
+++ src/phpqrcode.php
@@ -122,7 +122,7 @@
     define('QR_FIND_FROM_RANDOM', 2);                                                       // if false, checks all masks available, otherwise value tells count of masks need to be checked, mask id are got randomly
     define('QR_DEFAULT_MASK', 2);                                                               // when QR_FIND_BEST_MASK === false
                                                   
-    define('QR_PNG_MAXIMUM_SIZE',  1024);                                                       // maximum allowed png image width (in pixels), tune to make sure GD and PHP can handle such big images
+    define('QR_PNG_MAXIMUM_SIZE',  4096);                                                       // maximum allowed png image width (in pixels), tune to make sure GD and PHP can handle such big images
                                                   
 
 
